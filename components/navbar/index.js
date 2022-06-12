041ab108830c60b7bc05c341747906b8726f4ced
luxamrown <luxamrownn@gmail.com>
2022-06-13 06:22:09 +0700

    fix portfolio head

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -5,7 +5,7 @@ import useDimension from "../../utils";
 import Link from 'next/link'
 
 
-export default function NavPage() {
+export default function NavPage({href}) {
     const isMobile = useDimension(768)
     const router = useRouter()
   return (
@@ -19,17 +19,17 @@ export default function NavPage() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className={!isMobile?'nav-desktop':''}>
                     <Nav className="m-5 text-center">
-                        <Link href="/">
-                            <Nav.Link href="/" className={"mx-4 " +(router.pathname === '/'?'nav-text-active':'nav-text')}><h5>Home</h5></Nav.Link>
+                        <Link href="/" passHref>
+                            <Nav.Link href={href} className={"mx-4 " +(router.pathname === '/'?'nav-text-active':'nav-text')}><h5>Home</h5></Nav.Link>
                         </Link>
-                        <Link href="/about">
-                            <Nav.Link href="/about" className={"mx-4 " +(router.pathname === '/about'?'nav-text-active':'nav-text')}><h5>About</h5></Nav.Link>
+                        <Link href="/about" passHref>
+                            <Nav.Link href={href} className={"mx-4 " +(router.pathname === '/about'?'nav-text-active':'nav-text')}><h5>About</h5></Nav.Link>
                         </Link>
-                        <Link href="/portofolio">
-                            <Nav.Link href="/portofolio" className={"mx-4 " +(router.pathname === '/portofolio/[type]'?'nav-text-active':'nav-text')}><h5>Portfolio</h5></Nav.Link>
+                        <Link href="/portofolio" passHref>
+                            <Nav.Link href={href} className={"mx-4 " +(router.pathname === '/portofolio/[type]'?'nav-text-active':'nav-text')}><h5>Portfolio</h5></Nav.Link>
                         </Link>
-                        <Link href="/blog">
-                            <Nav.Link href="/blog" className={"mx-4 " +(router.pathname === '/blog'?'nav-text-active':'nav-text')}><h5>Blog</h5></Nav.Link>
+                        <Link href="/blog" passHref>
+                            <Nav.Link href={href} className={"mx-4 " +(router.pathname === '/blog'?'nav-text-active':'nav-text')}><h5>Blog</h5></Nav.Link>
                         </Link>
                     </Nav>
                 </Navbar.Collapse>
--- components/portofolio/index.js
+++ components/portofolio/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/dist/client/router'
 import { Button } from 'react-bootstrap'
+import Head from 'next/head'
 
 export default function Portofolio() {
   const router = useRouter()
@@ -9,9 +10,11 @@ export default function Portofolio() {
 
   return (
     <>
+    <Head>
     <title>
     Mohamad El Abror | Portfolio
     </title>
+    </Head>
       <div className='text-center portfolio-crossing'>
           <Button onClick={handleRenderPage} variant="outline-light text-secondary border-0 border-start border-end portfolio-crossing-text m-2" className='animation-text-right'>3D Renders</Button>
           <Button variant="outline-light border-0 border-start border-end portfolio-crossing-text m-2" className='animation-text'><a className='text-decoration-none text-secondary' rel='noreferrer' target="_blank" href="https://github.com/luxamrown">Code</a></Button>
