0932d0fa6632184264e2dae9f9d53ab3dba92fe5
mohamadelabror <mohamadelabror70@gmail.com>
2022-06-13 11:28:48 +0700

    update meta

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="description" content="Hello! My name is El-Abror"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,500;0,600;1,200;1,500;1,600&display=swap" rel="stylesheet"></link>
+        <meta name="keywords" content="mohamadelabror, mohamad, el, abror, developer, frontend, designer"></meta>
+        <meta name="author" content='el-abror'></meta>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
