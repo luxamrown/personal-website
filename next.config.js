2cb1a7204eb7736ec7ba74f7712b92fabacacff4
luxamrown <luxamrownn@gmail.com>
2022-06-16 20:42:13 +0700

    [UPDATE] loading component

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    URL_GET_ALL_POST: "https://go-cms-api.herokuapp.com/article/all",
-    URL_GET_DETAIL_POST: "https://go-cms-api.herokuapp.com/article"
-  }
-}
